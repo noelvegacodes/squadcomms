8a77979d06311b52748bd37d2194790fe95a1b5b
noelvegacodes <noelvegacodes@gmail.com>
2023-08-07 17:51:39 -0400

    global flash subscription and toasts

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,24 @@
 <script>
 	import '../app.postcss';
 	import './styles.css';
-</script>
+	import { getFlash } from 'sveltekit-flash-message/client';
+	import { page } from '$app/stores';
+	import toast, { Toaster } from 'svelte-french-toast';
 
+	const flash = getFlash(page);
+
+
+	flash.subscribe(($flash) => {
+	if (!$flash) return;
+
+	toast($flash.message, {
+		icon: $flash.type == 'success' ? '✅' : '❌', className: "bg-white", position: "bottom-right"
+	});
+
+	// Clearing the flash message could sometimes
+	// be required here to avoid double-toasting.
+	flash.set(undefined);
+	});
+</script>
+<Toaster />
 <slot />
